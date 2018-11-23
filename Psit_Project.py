3f15320606d2fbd9b68fe045f888093a1fd64fda
DoubleR0 <42911836+DoubleR0@users.noreply.github.com>
2018-11-23 21:24:47 +0700

    Step 3
    เปลี่ยน column ที่จะใช้เป็น int ให้แล้ว

--- Psit_Project.py
+++ Psit_Project.py
@@ -10,5 +10,12 @@ def main():
     type_data = data["Type"]
     price_data = data["Price"]
     genres_data = data["Genres"]
+    install_data = [i.replace("+", "").replace(",", "") for i in install_data]
+    install_data = list(map(int, install_data))
+    review_data = list(map(int, review_data))
+    price_data = list(map(int, price_data))
+    print(install_data)
+    print(min(install_data))
+    #print(data)
 main()
 
