e585a07dbf96ed311491bfbff4246f807154f7db
DoubleR0 <42911836+DoubleR0@users.noreply.github.com>
2018-11-23 20:46:13 +0700

    Step 2
    ดึงข้อมูลที่จะใช้มาหมดแล้ว

--- Psit_Project.py
+++ Psit_Project.py
@@ -2,6 +2,13 @@
 import pandas as pd
 def main():
     data = pd.read_csv("googleplaystore.csv", encoding="ISO-8859-1")
-    print(data)
+    name_data = data["App"]
+    rating_data = data["Rating"]
+    review_data = data["Reviews"]
+    size_data = data["Size"]
+    install_data = data["Installs"]
+    type_data = data["Type"]
+    price_data = data["Price"]
+    genres_data = data["Genres"]
 main()
 
