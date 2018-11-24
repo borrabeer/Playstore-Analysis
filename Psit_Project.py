c51a27205c073737688df3384347eeedb9af1e24
DoubleR0 <42911836+DoubleR0@users.noreply.github.com>
2018-11-24 21:49:38 +0700

    Step 4.1

--- Psit_Project.py
+++ Psit_Project.py
@@ -5,6 +5,7 @@ def main():
     data = pd.read_csv("googleplaystore.csv", encoding="ISO-8859-1")
     name_data = data["App"]         # ดึงข้อมูลมาใหเหมดแล้วที่จะใช้
     rating_data = data["Rating"]
+    name_rating_data_dict = name_rating(name_data, rating_data)
     review_data = data["Reviews"]
     size_data = data["Size"]
     install_data = data["Installs"]
@@ -13,14 +14,23 @@ def main():
     genres_data = data["Genres"]
     install_data = [i.replace("+", "").replace(",", "") for i in install_data]  #ทำให้ install_data เป็มเลขกลมๆ
     price_data = [i.replace("$", "") for i in price_data]  #ทำเช่นเดียวกับ install_data
+    #print(data)
     min_install, max_install = install(install_data) #นำ install_data เข้า function install จะหายอด install สูงสุดและต่ำสุด
     print(min_install, max_install)
+    print(name_rating_data_dict)
     #ช่วยหาindexของ max, min install ให้หน่อย
 def install(install_data):
     """" this function for max and min install """
     #นับจำนวนของ max, min install
-    count_min_install = install_data.count(min(install_data)) 
+    count_min_install = install_data.count(min(install_data))
     count_max_install = install_data.count(max(install_data))
     return count_min_install, count_max_install
+def name_rating(name_data, rating_data):
+    """ เอา rating มาทำเป็น dic """
+    data_dict = dict()
+    index = 0
+    for i in rating_data:
+        data_dict.update({name_data[index]:i})
+        index += 1
+    return data_dict
 main()
-
