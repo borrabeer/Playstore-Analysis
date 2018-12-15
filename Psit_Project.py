703b1c4ece1ce9f10f0bd6658c5ba08af38fee40
Double_R <rewkung43@gmail.com>
2018-12-15 17:24:32 +0700

    0

--- Psit_Project.py
+++ Psit_Project.py
@@ -8,48 +8,48 @@ def main():
     name_data = data["App"]         # ดึงข้อมูลมาให้หมดแล้วที่จะใช้
     rating_data = data["Rating"]
     category_data = data["Category"]
-    name_rating_data_dict = name_rating(name_data, rating_data)
-    category_rating_data = category_rating(category_data, rating_data)
+    #name_rating_data_dict = name_rating(name_data, rating_data)
+    #category_rating_data = category_rating(category_data, rating_data)
     review_data = data["Reviews"]
     size_data = data["Size"]
     install_data = data["Installs"]
     type_data = data["Type"]
     price_data = data["Price"]
     genres_data = data["Genres"]
-    install_data = [i.replace("+", "").replace(",", "") for i in install_data]  #ทำให้ install_data เป็มเลขกลมๆ
-    install_data = [int(i) for i in install_data]
-    price_data = [i.replace("$", "") for i in price_data]  #ทำเช่นเดียวกับ install_data
+    #install_data = [i.replace("+", "").replace(",", "") for i in install_data]  #ทำให้ install_data เป็มเลขกลมๆ
+    #install_data = [int(i) for i in install_data]
+    #price_data = [i.replace("$", "") for i in price_data]  #ทำเช่นเดียวกับ install_data
+    category_review_data = category_review(category_data, review_data)
     #print(data)
-    number_min, number_max = install(install_data) #หา index ของค่า max, min
+    #number_min, number_max = install(install_data) #หา index ของค่า max, min
     #หาชื่อของ app
-    min_name_data = find_name(number_min, name_data)
-    max_name_data = find_name(number_max, name_data)
+    #min_name_data = find_name(number_min, name_data)
+    #max_name_data = find_name(number_max, name_data)
     # print(max(install_data))
     # print(number_min)
     # print(number_max)
     # print(min_name_data)
     # print(max_name_data)
     # print(category_data)
-    category_review_data = category_review(category_data, review_data)
     print(category_review_data)
-    count_rating_data = count_rating(rating_data)
-    custom_style = Style(
-    background='transparent',
-    plot_background='transparent',
-    opacity='.6',
-    opacity_hover='.9',
-    transition='400ms ease-in',
-    colors=('#F102A1',))
-    rating_chart = pg.Bar(fill=True, style=custom_style)
-    rating_chart.title = "Rating in Google Playstore"
-    rating_chart.x_labels = count_rating_data.keys()
-    rating_chart.add("Rating", count_rating_data.values())
-    rating_chart.render_to_file("rating_chart.svg")
-    category_rating_chart = pg.HorizontalBar()
-    category_rating_chart.title = "Average Rating of Categories"
-    category_rating_chart.x_labels = category_rating_data.keys()
-    category_rating_chart.add("Average Ratings", category_rating_data.values())
-    category_rating_chart.render_to_file("category_rating_chart.svg")
+    #count_rating_data = count_rating(rating_data)
+    #custom_style = Style(
+    #background='transparent',
+    #plot_background='transparent',
+    #opacity='.6',
+    #opacity_hover='.9',
+    #transition='400ms ease-in',
+    #colors=('#F102A1',))
+    #rating_chart = pg.Bar(fill=True, style=custom_style)
+    #rating_chart.title = "Rating in Google Playstore"
+    #rating_chart.x_labels = count_rating_data.keys()
+    #rating_chart.add("Rating", count_rating_data.values())
+    #rating_chart.render_to_file("rating_chart.svg")
+    #category_rating_chart = pg.HorizontalBar()
+    #category_rating_chart.title = "Average Rating of Categories"
+    #category_rating_chart.x_labels = category_rating_data.keys()
+    #category_rating_chart.add("Average Ratings", category_rating_data.values())
+    #category_rating_chart.render_to_file("category_rating_chart.svg")
 def install(install_data):
     """" ฟังชั่นนี้ หายอดติดตั้งที่มากที่สุดและน้อยที่สุดใน google playstore """
     #นับจำนวนของ max, min install
@@ -126,7 +126,7 @@ def category_review(category, review):
     data_lst = list()
     for i in category:
         for j in review:
-            data_lst.append([category, review])
+            data_lst.append([i, j])
     for i in data_lst:
         if i[0] not in data_dict:
             data_dict.update({i[0]:[i[1]]})
