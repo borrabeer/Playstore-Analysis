ff8a9beb65dcd428021c46ec7f6db00d8013d785
Worapath Pakkavesa <61070198@kmitl.ac.th>
2018-12-15 19:21:50 +0700

    Editing python file
    editing python file for readable and simple to understand.

--- Psit_Project.py
+++ Psit_Project.py
@@ -1,104 +1,76 @@
-""" Psit Project """
+""" Playstore-Analysis Project """
 import pandas as pd
 import pygal as pg
 from pygal.style import Style, DarkSolarizedStyle
 def main():
-    """ main function for input data """
-    data = pd.read_csv("googleplaystore.csv", encoding="ISO-8859-1")
-    name_data = data["App"]         # ดึงข้อมูลมาให้หมดแล้วที่จะใช้
-    rating_data = data["Rating"]
-    category_data = data["Category"]
-    name_rating_data_dict = name_rating(name_data, rating_data)
-    category_rating_data = category_rating(category_data, rating_data)
-    review_data = data["Reviews"]
-    size_data = data["Size"]
-    size_data = [i.replace("M", "") for i in size_data] #ทำให้ data ของ Size เป็นตัวเลขทั้งหมด
-    size_data = [i.replace("Varies with device", "0") for i in size_data]
-    size_data = [i.replace("+", "") for i in size_data]
-    size_data = [i.replace(',', '') for i in size_data]
-    for i in range(len(size_data)):
+    """ main function for data and making charts """
+    """
+
+    Pulling Data from CSV file
+
+    """
+    data = pd.read_csv("googleplaystore.csv", encoding="ISO-8859-1") #อ่านไฟล์ csv
+    name_data = data["App"] #data ชื่อแอพพลิเคชั่น
+    rating_data = data["Rating"] #data rating ของแต่ละแอพพลิเคชั่น
+    category_data = data["Category"] #data หมวดหมู่ของแต่ละแอพพลิเคชั่น
+    review_data = data["Reviews"] #data reviews ของแต่ละแอพพลิเคชั่น
+    size_data = data["Size"] #data ขนาดของแต่ละแอพพลิเคชั่น
+    install_data = data["Installs"] #data ยอดดาวน์โหลดของแต่ละแอพพลิเคชั่น
+    """
+
+    Editing data into readable data
+
+    """
+    size_data = [i.replace("M", "").replace("Varies with device", "0").replace("+", "").replace(',', '') for i in size_data] #ทำให้ data ของ Size เป็นตัวเลขทั้งหมด
+    for i in range(len(size_data)): #ทำให้ขนาดของ data เป็นหน่วยเดียวกัน
         if 'k' in size_data[i]:
             size_data[i] = size_data[i].replace('k', "")
             size_data[i] = int(float(size_data[i]))
             size_data[i] /= 1024
     size_data = [float(i) for i in size_data]
-    category_size_data = category_size(category_data, size_data)
-    install_data = data["Installs"]
-    install_data = [i.replace("+", "").replace(",", "") for i in install_data]  #ทำให้ install_data เป็มเลขกลมๆ
+    install_data = [i.replace("+", "").replace(",", "") for i in install_data]  #ทำให้ data ของยอดดาวน์โหลดเป็นตัวเลขทั้งหมด
     install_data = [int(i) for i in install_data]
+    price_data = [i.replace("$", "") for i in price_data]
+    """
+
+    Using function and returning data in to variables
+
+    """
+    category_review_data = category_review(category_data, review_data)
+    category_rating_data = category_rating(category_data, rating_data)
     name_install_data = name_install(name_data, install_data)
-    type_data = data["Type"]
-    price_data = data["Price"]
-    genres_data = data["Genres"]
-    price_data = [i.replace("$", "") for i in price_data]  #ทำเช่นเดียวกับ install_data
-    category_review(category_data, review_data)
-    # category_review_data = category_review(category_data, review_data)
-    # number_min, number_max = install(install_data) #หา index ของค่า max, min
-    # #หาชื่อของ app
-    # min_name_data = find_name(number_min, name_data)
-    # max_name_data = find_name(number_max, name_data)
+    category_size_data = category_size(category_data, size_data)
+    """
+    
+    Making Average reviews of each Categories chart
+
+    """
+    category_review_chart = pg.Bar()
+    category_review_chart.title = "Average Reviews of each Categories"
+    for i in category_review_data:
+        category_review_chart.add(i, category_review_data[i])
+    category_review_chart.render_to_file("category_review_chart.svg")
+    """
+
+    Making Average ratings of each Categories chart
+
+    """
     category_rating_chart = pg.HorizontalBar()
-    category_rating_chart.title = "Average Rating of each Categories"
+    category_rating_chart.title = "Average Ratings of each Categories"
     for i in category_rating_data:
         category_rating_chart.add(i, category_rating_data[i])
     category_rating_chart.render_to_file("category_rating_chart.svg")
+    """
+
+    Making a Average sizes of each Categories chart
+
+    """
     category_size_chart = pg.Bar(style=DarkSolarizedStyle)
-    category_size_chart.title = "Average Size of Application of each Category(MBs)"
+    category_size_chart.title = "Average Sizes of Application of each Categories(MBs)"
     for i in category_size_data.keys():
         category_size_chart.add(i, category_size_data[i])
     category_size_chart.render_to_file("category_size_chart.svg")
-def install(install_data):
-    """" ฟังชั่นนี้ หายอดติดตั้งที่มากที่สุดและน้อยที่สุดใน google playstore """
-    #นับจำนวนของ max, min install
-    number_min = list()
-    number_max = list()
-    for i in range(len(install_data)):
-        if install_data[i] == min(install_data):
-            number_min.append(i)
-        if install_data[i] == max(install_data):
-            number_max.append(i)
-    return number_min, number_max
-def name_rating(name_data, rating_data):
-    """  หา rating ของแต่ละแอพ แล้วมาทำ dict เพื่อให้ง่ายต่อการคนหา  """
-    #เอา name-data กับ rating-data มาทำ dict
-    data_dict = dict()
-    index = 0
-    for i in rating_data:
-        data_dict.update({name_data[index]:i})
-        index += 1
-    return data_dict
-def find_name(number, name_data):
-    """ รวมแอพที่มีชื่อซ้ำ แต่คนละหมวดหมู่ ให้เป็นชื่อเดียว """
-    set_name = set()
-    for i in number:
-        set_name.add(name_data[int(i)])
-    return set_name
-def count_rating(rating_data):
-    """ นับค่า rating ของแต่ละแอฟ ออกมาเป็นช่วงตัวเลข """
-    #นับค่า Rating ของแต่ละ แอพพลิเคชั่นออกมาเป็นช่วงของตัวเลข
-    count_rating = { 5 : 0, 4.5 : 0, 4 : 0, 3.5 : 0, 3 : 0, 2.5 : 0, 2 : 0, 1.5 : 0, 1 : 0, 0 : 0}
-    for i in rating_data:
-        if i == 5:
-            count_rating[5] += 1
-        elif i >= 4.5:
-            count_rating[4.5] += 1
-        elif i >= 4:
-            count_rating[4] += 1
-        elif i >= 3.5:
-            count_rating[3.5] += 1
-        elif i >= 3:
-            count_rating[3] += 1
-        elif i >= 2.5:
-            count_rating[2.5] += 1
-        elif i >= 2:
-            count_rating[2] += 1
-        elif i >= 1.5:
-            count_rating[1.5] += 1
-        elif i >= 1:
-            count_rating[1] += 1
-        else:
-            count_rating[0] += 1
-    return count_rating
+
 def category_rating(category, rating):
     """นำ rating ของ category มาหาค่าเฉลี่ย"""
     category_rating_data = {}
@@ -121,16 +93,19 @@ def category_review(category, review):
     """ นำ review และ category มาทำ dict """
     data_dict = dict()
     data_lst = list()
-    for i in category:
-        for j in review:
-            data_lst.append([i, j])
+    for i in range(len(category)):
+            data_lst.append([category[i], review[i]])
     for i in data_lst:
         if i[0] not in data_dict:
             data_dict.update({i[0]:[i[1]]})
         else:
-            lst1 = data_dict[i]
+            lst1 = data_dict[i[0]]
             lst1.append(i[1])
-            data_dict.update({i:lst1})
+            data_dict.update({i[0]:lst1})
+    for i in data_dict:
+        lst1 = data_dict[i]
+        ava = sum(lst1)/len(lst1)
+        data_dict.update({i:ava})
     return data_dict
 def category_size(category, size):
     """นำขนาดของแต่ละแอพพลิเคชั่นในแต่ละหมวดมาหาค่าเฉลี่ย"""
@@ -159,22 +134,4 @@ def name_install(name, install):
             name_install_data[name[index]] = i
         index += 1
     return name_install_data
-def category_review(category, review):
-    """ นำ review และ category มาทำ dict """
-    data_dict = dict()
-    data_lst = list()
-    for i in range(len(category)):
-            data_lst.append([category[i], review[i]])
-    for i in data_lst:
-        if i[0] not in data_dict:
-            data_dict.update({i[0]:[i[1]]})
-        else:
-            lst1 = data_dict[i[0]]
-            lst1.append(i[1])
-            data_dict.update({i[0]:lst1})
-    for i in data_dict:
-        lst1 = data_dict[i]
-        ava = sum(lst1)/len(lst1)
-        data_dict.update({i:ava})
-    print(data_dict)
 main()
